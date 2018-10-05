19c03b51e2214e0a0c0cde461a8ab7023427a04f
Borko Rajkovic <borko.rajkovic@limitforex.com>
2018-10-05 13:28:40 +0200

    Display user

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-login',
