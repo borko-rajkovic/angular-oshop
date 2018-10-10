f3bba39ab9d180a3bcf8143b350468a4c771940e
Borko <rajkovicborko@gmail.com>
2018-10-11 00:16:55 +0200

    Searching for product

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,27 +1,42 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/products';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
   products$;
-  products;
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
     this.products$ = productService.getAll();
-    this.products$.subscribe(x => {
+    this.subscription = this.products$.subscribe(x => {
       this.products = [];
       Object.keys(x).forEach(key => {
-        this.products.push({ key, title: x[key].title, price: x[key].price });
+        this.products.push({ key, title: x[key].title, price: x[key].price, category: x[key].category, imageUrl: x[key].imageUrl });
       });
+      this.filteredProducts = this.products;
       console.log(this.products);
     });
   }
 
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
