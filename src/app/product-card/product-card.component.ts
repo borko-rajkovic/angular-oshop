25c38e2bd2da3a19212e4a2eaca7d5cace4546f6
Borko Rajkovic <borko.rajkovic@limitforex.com>
2018-10-12 14:32:25 +0200

    Refactor Product Card

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,15 @@
+import { Product } from 'src/app/models/products';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+  @Input() product: Product;
+  @Input() showActions = true;
+
+  constructor() { }
+
+}
