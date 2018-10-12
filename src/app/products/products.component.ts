5d09aba190769e6b4e3d14a9b2fde2ae78c39a66
Borko Rajkovic <borko.rajkovic@limitforex.com>
2018-10-12 12:26:56 +0200

    Displaying the products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../models/products';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products: Product[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(productService: ProductService) {
+    productService.getAll().subscribe(x => {
+      this.products = [];
+      Object.keys(x).forEach(key => {
+        this.products.push({
+          key,
+          title: x[key].title,
+          price: x[key].price,
+          category: x[key].category,
+          imageUrl: x[key].imageUrl
+        });
+      });
+    });
   }
 
 }
